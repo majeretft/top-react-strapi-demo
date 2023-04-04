826951e63688c7db74c1654daca7eeaf048b7d50
Andrey Bliznev <majeretft@mail.ru>
2023-04-04 13:32:20 +0300

    update entry

--- src/store/card.js
+++ src/store/card.js
@@ -21,36 +21,50 @@ export const getCards = createAsyncThunk("cards/getCards", async () => {
   return response.data;
 });
 
-export const updateCard = createAsyncThunk("cards/updateCard", async (data, thunkApi) => {
-  // TODO Сначала загрузить картинку, а потом обновлять ID для фото
-  
-  const state = thunkApi.getState();
+export const updateCard = createAsyncThunk(
+  "cards/updateCard",
+  async (data, thunkApi) => {
+    const state = thunkApi.getState();
 
-  const fd = new FormData();
+    const fd = new FormData();
+    fd.append("files", data.cardImage);
 
-  const dt = {
-    cardHeader: data.cardHeader,
-    cardBody: data.cardBody,
-  };
+    const responseUpload = await axios.post(
+      `${constants.urlBase}/${constants.apiPath}/upload`,
+      fd,
+      {
+        validateStatus: null,
+        headers: {
+          Authorization: `bearer  ${state.user.userData.jwt}`,
+        },
+      }
+    );
 
-  fd.append("data", JSON.stringify(dt));
-  fd.append("files.photo", data.cardImage, data.cardImage.name);
+    if (responseUpload.data?.error)
+      throw JSON.stringify(responseUpload.data?.error);
 
-  const response = await axios.put(
-    `${constants.urlBase}/${constants.apiPath}/cards/${data.id}?populate=*`,
-    fd,
-    {
-      validateStatus: null,
-      headers: {
-        Authorization: `bearer  ${state.user.userData.jwt}`,
+    const response = await axios.put(
+      `${constants.urlBase}/${constants.apiPath}/cards/${data.id}?populate=*`,
+      {
+        data: {
+          cardHeader: data.cardHeader,
+          cardBody: data.cardBody,
+          cardImage: responseUpload.data[0].id,
+        },
       },
-    }
-  );
+      {
+        validateStatus: null,
+        headers: {
+          Authorization: `bearer  ${state.user.userData.jwt}`,
+        },
+      }
+    );
 
-  if (response.data?.error) throw JSON.stringify(response.data?.error);
+    if (response.data?.error) throw JSON.stringify(response.data?.error);
 
-  return response.data;
-});
+    return response.data;
+  }
+);
 
 export const cardSlice = createSlice({
   name: "card",
@@ -66,6 +80,28 @@ export const cardSlice = createSlice({
       state.loading = true;
     },
     [getCards.rejected]: (state, action) => {
+      // TODO
+
+      state.loading = false;
+    },
+
+    [updateCard.fulfilled]: (state, action) => {
+      const index = state.cards.findIndex(
+        (x) => x.id === action.payload.data.id
+      );
+
+      state.loading = false;
+
+      if (index < 0) return;
+
+      state.cards[index] = action.payload.data;
+
+      state.loading = false;
+    },
+    [updateCard.pending]: (state) => {
+      state.loading = true;
+    },
+    [updateCard.rejected]: (state, action) => {
       // TODO
 
       state.loading = false;
